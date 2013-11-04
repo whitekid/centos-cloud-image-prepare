8083e306e467ece71bbc375ce4f5c7449110d6db
Choe, Cheng-Dae <whitekid@gmail.com>
2013-11-04 15:05:47 +0900

    supports ubuntu

--- prepare.sh
+++ prepare.sh
@@ -1,79 +1,98 @@
 #!/bin/bash
 cloud_user=${CLOUD_USER:-ec2-user}
-chkconfig iptables off
-release=`cut -d ' ' -f 3 /etc/redhat-release`
 
-use_cloud_util=yes
-case $release in
-	5.8)
-		epel_pkg=http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/5/i386/epel-release-5-4.noarch.rpm
-		use_cloud_util=no
-		;;
-	6.*)
-		epel_pkg=http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/i386/epel-release-6-8.noarch.rpm
-		;;
-	*)
-		echo "Unsupported release $release"
-		exit
-esac
-
-if ! rpm -qa | grep -q epel; then
-	rpm -Uvh $epel_pkg
+if [ -f /etc/redhat-release ]; then
+	REL=`cat /etc/redhat-release | awk '{print $3}'`
+	OS='CentOS'
+else
+	REL=`lsb_release -sr`
+	OS=`lsb_release -si`
 fi
 
-if [ $use_cloud_util == 'yes' ]; then
-	if ! which ec2metadata; then
-		yum install cloud-utils -y
-	fi
+# disable iptables
+if [ "$OS" == "CentOS" ]; then
+	chkconfig iptables off
 fi
 
-if ! which cloud-init; then
-	yum install cloud-init -y
-fi
-
-useradd -m -s `which bash` ${cloud_user}
-
-case $release in
-	5.8)
-		if ! grep -q "^${cloud_user} :ALL" /etc/sysconfig/network; then
-			echo "${cloud_user} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+# cloud-init
+case "$OS" in
+	CentOS)
+		case $REL in
+			5.8)
+				epel_pkg=http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/5/i386/epel-release-5-4.noarch.rpm
+				use_cloud_util=no
+				;;
+			6.*)
+				epel_pkg=http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+				;;
+			*)
+				echo "Unsupported release $release"
+				exit
+		esac
+		if ! rpm -qa | grep -q epel; then
+			rpm -Uvh $epel_pkg
 		fi
+		
+		if [ $use_cloud_util == 'yes' ]; then
+			which ec2metadata || yum install cloud-utils -y
+		fi
+
+		which cloud-init || yum install cloud-init -y
 		;;
-	6.*)
-		echo "${cloud_user} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${cloud_user}
-		chmod 0660 /etc/sudoers.d/${cloud_user}
+
+	Ubuntu)
+		which cloud-init || apt-get install -y cloud-init
 		;;
 esac
 
-cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
+# Network settings
+case "$OS" in
+	CentOS)
+		cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
 DEVICE=eth0
 TYPE=Ethernet
 ONBOOT=yes
 BOOTPROTO=dhcp
 EOF
+		if ! grep 'NOZEROCONF=yes' /etc/sysconfig/network; then
+			echo "NOZEROCONF=yes" >> /etc/sysconfig/network
+		fi
+		;;
+	Ubuntu)
+		cat > /etc/network/interfaces << EOF
+auto lo
+iface lo inet lookback
+
+auto eth0
+iface eth0 inet dhcp
+EOF
+		;;
+esac
 
 # refer http://jcape.name/2009/07/17/distributing-static-routes-with-dhcp/
-if [ "$release" = "5.8" ]; then
+if [ "$OS" == "CentOS" && "$REL" == "5.8" ]; then
 	curl -s https://raw.github.com/whitekid/centos-cloud-image-prepare/master/dhclient.conf > /etc/dhclient.conf
 	curl -s https://raw.github.com/whitekid/centos-cloud-image-prepare/master/dhclient-exit-hooks > /etc/dhclient-exit-hooks
 fi
 
+# remove persistents things
 rm -f /etc/udev/rules.d/70-persistent-*.rules
 rm -f /etc/ssh/ssh_host*
-rm -rf /home/$CLOUD_USER/.ssh
+rm -rf /home/$cloud_user/.ssh
 rm -rf /root/.ssh
-
-if ! grep 'NOZEROCONF=yes' /etc/sysconfig/network; then
-	echo "NOZEROCONF=yes" >> /etc/sysconfig/network
-fi
-
 rm -rf /var/lib/cloud
 rm -f /etc/resolv.conf
 yum clean all
+apt-get clean all
 
+# clear history
+history -c
+rm -f ~/.bash_history
+rm -f /root/.bash_history
+rm -f /home/$cloud_user/.bash_history
+
+# wipe free space
 dd if=/dev/zero of=file
 rm -f file
 
-history -c
-rm -f ~/.bash_history
-
+echo "Please shutdown"
